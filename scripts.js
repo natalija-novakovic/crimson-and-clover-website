0c3f4a188b356e5b62d744d33fd625a6e1036988
natalija-novakovic <natalijadsgn@gmail.com>
2023-09-23 18:27:17 +0200

    Ispravke

--- scripts.js
+++ scripts.js
@@ -272,3 +272,24 @@ categoryButtons.forEach(button => {
         }
     });
 });
+
+// BACK TO TOP BUTTON
+// Get the button:
+let mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 1080px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1080 || document.documentElement.scrollTop > 1080) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
